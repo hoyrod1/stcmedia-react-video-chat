9ec4dd794813848e9235ba61707d7a40e94fb339
Rodney St Cloud <hoyrod1@aol.com>
2025-08-29 20:24:12 -0400

    Removed folder from repository

--- videochat-front-end/src/redux-elements/actions/updateCallStatus.js
+++
@@ -1,6 +0,0 @@
-export default (prop, value) => {
-  return {
-    type: "UPDATE_CALL_STATUS",
-    payload: { prop, value },
-  };
-}; //
--- videochat-front-end/src/videoComponents/ChatWindow.js
+++
@@ -1,9 +0,0 @@
-const ChatWindow = () => {
-  return (
-    <div className={`chat-window`}>
-      <h1>Chat</h1>
-    </div>
-  );
-};
-
-export default ChatWindow;
